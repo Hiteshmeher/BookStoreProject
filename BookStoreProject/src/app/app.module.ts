f47268c3e69fcc47c8fb674b8f68977db5b3f9f3
Hiteshmeher <meher.hitesh4@gmail.com>
2022-11-19 22:37:31 +0530

    hiii

--- BookStoreProject/src/app/app.module.ts
+++ BookStoreProject/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HomeComponent } from './component/home/home.component';
